93bb0c5e59c3ff8f9a93f2852c81bd7c40b74252
jsujeethR18 <jsujeeth8712@gmail.com>
2025-09-18 15:39:20 -0500

    first commit

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM python:3.9
+
+RUN pip install pandas requests python-dotenv psycopg2 dagster dagster-webserver dagit dagster_postgres
+
+WORKDIR /app
+ENV DAGSTER_HOME=/app
+
+COPY dagster.yaml .
+COPY workspace.yaml .
+COPY Datasets /app/Datasets
+COPY .env /app/
+COPY dags/city_sampler.py /app/
+COPY dags/data_ingestion.py /app/
+COPY dags/generate_sql_inserts.py /app/
+COPY dags/repository.py /app/dags/repository.py
+
+EXPOSE 3000
+
+CMD ["dagster-webserver", "-w", "workspace.yaml", "-h", "0.0.0.0", "-p", "3000"]
---
+++ dags/repository.py
@@ -0,0 +1,41 @@
+from city_sampler import select_cities_to_csv
+from data_ingestion import generate_weather_data
+from generate_sql_inserts import generate_country_sql_statements, generate_city_sql_statements
+from generate_sql_inserts import generate_city_country_statements_asset, generate_date_sql_statements
+from generate_sql_inserts import generate_bike_sql_statements, ingest_data
+from dagster import repository
+from dagster import (
+    AssetSelection,
+    Definitions,
+    ScheduleDefinition,
+    define_asset_job,
+
+)
+
+all_assets = [
+    select_cities_to_csv,
+    generate_weather_data,
+    generate_country_sql_statements,
+    generate_city_sql_statements,
+    generate_city_country_statements_asset,
+    generate_date_sql_statements,
+    generate_bike_sql_statements,
+    ingest_data
+]
+
+#initialize dagster job
+data_ingestion_job = define_asset_job("data_ingestion_job", selection=AssetSelection.all())
+
+#initialize dagster schedule that runs the job every minute (for testing purposes)
+ingestion_schedule = ScheduleDefinition(
+    job=data_ingestion_job,
+    cron_schedule="*/5 * * * *",  # every 5 minute
+)
+
+@repository
+def my_repository():
+    return [
+        data_ingestion_job,
+        ingestion_schedule,
+        *all_assets
+    ]
